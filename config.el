c66d8d3a5b7d4c9e745dd69a9cd70a2cc1ba212a
nosferalatu <farrell_david@yahoo.com>
2014-02-24 11:17:20 -0800

    Fixed server start error

--- config.el
+++ config.el
@@ -13,6 +13,13 @@
 (setq inhibit-splash-screen t)
 (setq initial-buffer-choice 'none)
 
+;; Start Emacs server
+;; Suppress error "directory ~/.emacs.d/server is unsafe" on Windows
+;; http://stackoverflow.com/questions/885793/emacs-error-when-calling-server-start
+(require 'server)
+(when (and (>= emacs-major-version 23)
+           (equal window-system 'w32))
+  (defun server-ensure-safe-dir (dir) "Noop" t))
 (server-start)
 
 (require 'cl)
